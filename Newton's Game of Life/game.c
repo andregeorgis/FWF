2785214250c4dc60ebbffe48006a5d29e40834c5
andregeorgis <andregeorgis@gmail.com>
2020-01-23 07:46:41 +1100

    Created new project (Conway's Game of Life) and wrote basic skeleton code

---
+++ Newton's Game of Life/game.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// Generate the matrix of cells - start small for testing then go big
+// You can change function signature (i.e. the int**) if you think its better
+// Matrix should have either a 1 (alive cell) or 0 (dead cell)
+// Maybe later we can use structs for cells/matrix
+int** setup() {
+  ; // Return the matrix of cells
+}
+
+// Display something like "O" for alive cell and " " for dead cell
+void display() {
+  ;
+}
+
+// Determine the next generation of cells
+// Assume cells outside border of matrix are dead
+int** new_state(int** state) {
+  ; // Return the new matrix
+}
+
+// Implement the logic
+int main() {
+  ;
+}
