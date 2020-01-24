9952c708e6e7f8782787b013eb5eac5114aff549
jtran03 <60172315+jtran03@users.noreply.github.com>
2020-01-24 15:57:58 +1100

    Initialised the starting matrix

--- Newton's Game of Life/src/game.c
+++ Newton's Game of Life/src/game.c
@@ -1,27 +1,68 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <math.h>
+#include "matrix.h"
 // Generate the matrix of cells - start small for testing then go big
 // You can change function signature (i.e. the int**) if you think its better
 // Matrix should have either a 1 (alive cell) or 0 (dead cell)
 // Maybe later we can use structs for cells/matrix
-int** setup() {
-  ; // Return the matrix of cells
+int** setup(int size) {
+
+  int** array = NULL;
+  int i, j;
+
+  array = (int**)malloc(size*sizeof(int*));
+  for (i = 0; i < size; i++){
+    (array)[i] = (int*)malloc(size*sizeof(int));
+  }
+
+  for (i = 0; i < size; i++){
+    for (j = 0; j < size; j++){
+      array[i][j] = 0;
+    }
+  }
+
+  return array;
 }
 
 // Display something like "O" for alive cell and " " for dead cell
-void display() {
-  ;
+void display(int** array, int size) {
+
+  for (int i = 0; i < size; i++){
+    for (int j = 0; j < size; j++){
+      printf("%d ", array[i][j]);
+    }
+    printf("\n");
+  }
 }
 
 // Determine the next generation of cells
 // Assume cells outside border of matrix are dead
-int** new_state(int** state) {
-  ; // Return the new matrix
+int** new_state(int** initial_state, int length) {
+
+  int ** newmatrix = NULL;
+  newmatrix = setup(length);
+
 }
+
 
 // Implement the logic
 int main() {
-  ;
 
-  printf("hi\n");
+  int length = 3;
+  int i, j;
+
+  // Setting up the first generation//
+  int **oldmatrix = NULL;
+  oldmatrix = setup(length);
+
+  for (i = 0; i < length; i++){
+    for (j = 0; j < length; j++){
+      oldmatrix[i][j] = (rand() % 2);
+    }
+  }
+
+  display(oldmatrix, length);
+  printf("\n");
+
 }
