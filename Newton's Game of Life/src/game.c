21c2d2a8ae2e1788077688c3b099a23f66a356f9
jtran03 <60172315+jtran03@users.noreply.github.com>
2020-01-29 11:48:56 +1100

    Added the next generation loop. Press 'c' in terminal to access next generation Double tap enter to exit

--- Newton's Game of Life/src/game.c
+++ Newton's Game of Life/src/game.c
@@ -18,7 +18,7 @@ int** setup(int size) {
 
   for (i = 0; i < size; i++){
     for (j = 0; j < size; j++){
-      array[i][j] = 0;
+      array[i][j] = 8;
     }
   }
 
@@ -40,29 +40,89 @@ void display(int** array, int size) {
 // Assume cells outside border of matrix are dead
 int** new_state(int** initial_state, int length) {
 
-  int ** newmatrix = NULL;
+  int **newmatrix = NULL;
   newmatrix = setup(length);
 
+  int count = 0;
+  int i, j;
+
+  for (i = 0; i < length; i++){
+    for (j = 0; j < length; j++){
+
+      // Check for neighbourss//
+      if (i == 0 || i == length - 1 || j == 0 || j == length - 1){
+
+        newmatrix[i][j] = initial_state[i][j];
+
+      } else {
+
+            count = 0;
+            count += initial_state[i][j+1];
+            count += initial_state[i][j-1];
+            count += initial_state[i+1][j];
+            count += initial_state[i+1][j+1];
+            count += initial_state[i+1][j-1];
+            count += initial_state[i-1][j];
+            count += initial_state[i-1][j+1];
+            count += initial_state[i-1][j-1];
+
+            //Births: Each dead cell adjacent to exactly three live neighbors
+            //will become live in the next generation.
+            if (count == 3){
+              newmatrix[i][j] = 1;
+            }
+
+            //Death by isolation: Each live cell with one or fewer live
+            //neighbors will die in the next generation.
+            if (count < 2){
+              newmatrix[i][j] = 0;
+            }
+
+            //Death by overcrowding: Each live cell with four or more live
+            //neighbors will die in the next generation.
+            if (count > 3){
+              newmatrix[i][j] = 0;
+            }
+
+            //Survival: Each live cell with either two or three live neighbors
+            // will remain alive for the next generation.
+            if (count > 1 && count < 4){
+              newmatrix[i][j] = 1;
+            }
+      }
+    }
+  }
+
+  return newmatrix;
 }
 
 
 // Implement the logic
 int main() {
 
-  int length = 3;
+  int length = 10;
   int i, j;
 
   // Setting up the first generation//
   int **oldmatrix = NULL;
   oldmatrix = setup(length);
 
-  for (i = 0; i < length; i++){
-    for (j = 0; j < length; j++){
+  for (i = 1; i < length - 1; i++){
+    for (j = 1; j < length - 1; j++){
       oldmatrix[i][j] = (rand() % 2);
     }
   }
 
   display(oldmatrix, length);
+
   printf("\n");
 
+  // Setting up next generation//
+  char userinput[100];
+
+  while((fgets(userinput, sizeof userinput, stdin) != NULL) && (userinput[0] != '\n') && userinput[0] == 'c'){
+    int** newmatrix = new_state(oldmatrix, length);
+    display(newmatrix, length);
+    oldmatrix = newmatrix;
+  }
 }
