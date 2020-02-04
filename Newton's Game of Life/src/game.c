fa029f518ac5ff2a88568a09581a768a7ff4a082
andregeorgis <andregeorgis@gmail.com>
2020-02-04 11:07:17 +1100

    Changed matrix from int** to cell** where cell is just a struct for each cell - added explicit wall functionality (border for matrix)

--- Newton's Game of Life/src/game.c
+++ Newton's Game of Life/src/game.c
@@ -6,19 +6,31 @@
 // You can change function signature (i.e. the int**) if you think its better
 // Matrix should have either a 1 (alive cell) or 0 (dead cell)
 // Maybe later we can use structs for cells/matrix
-int** setup(int size) {
 
-  int** array = NULL;
+typedef struct cell {
+  int alive;
+  int wall;
+} cell;
+
+cell** setup(int size) {
+
+  cell** array = NULL;
   int i, j;
 
-  array = (int**)malloc(size*sizeof(int*));
+  array = (cell**)malloc(size*sizeof(cell*));
   for (i = 0; i < size; i++){
-    (array)[i] = (int*)malloc(size*sizeof(int));
+    (array)[i] = (cell*)malloc(size*sizeof(cell));
   }
 
   for (i = 0; i < size; i++){
     for (j = 0; j < size; j++){
-      array[i][j] = 0;
+      array[i][j].alive = 0;
+
+      if (i == 0 || i == size - 1 || j == 0 || j == size - 1){
+        array[i][j].wall = 1;
+      } else {
+        array[i][j].wall = 0;
+      }
     }
   }
 
@@ -26,11 +38,15 @@ int** setup(int size) {
 }
 
 // Display something like "O" for alive cell and " " for dead cell
-void display(int** array, int size) {
+void display(cell** array, int size) {
 
   for (int i = 0; i < size; i++){
     for (int j = 0; j < size; j++){
-      printf("%d ", array[i][j]);
+      if (array[i][j].wall) {
+        printf("W ");
+      } else {
+        printf("%d ", array[i][j].alive);
+      }
     }
     printf("\n");
   }
@@ -38,9 +54,9 @@ void display(int** array, int size) {
 
 // Determine the next generation of cells
 // Assume cells outside border of matrix are dead
-int** new_state(int** initial_state, int length) {
+cell** new_state(cell** initial_state, int length) {
 
-  int **newmatrix = NULL;
+  cell **newmatrix = NULL;
   newmatrix = setup(length);
 
   int count = 0;
@@ -49,46 +65,41 @@ int** new_state(int** initial_state, int length) {
   for (i = 0; i < length; i++){
     for (j = 0; j < length; j++){
 
-      // Check for neighbourss//
-      if (i == 0 || i == length - 1 || j == 0 || j == length - 1){
+      // Check for neighbours - if it is a wall it stays dead//
+      if (!initial_state[i][j].wall){
+        count = 0;
+        count += initial_state[i][j+1].alive;
+        count += initial_state[i][j-1].alive;
+        count += initial_state[i+1][j].alive;
+        count += initial_state[i+1][j+1].alive;
+        count += initial_state[i+1][j-1].alive;
+        count += initial_state[i-1][j].alive;
+        count += initial_state[i-1][j+1].alive;
+        count += initial_state[i-1][j-1].alive;
 
-        newmatrix[i][j] = initial_state[i][j];
+        //Births: Each dead cell adjacent to exactly three live neighbors
+        //will become live in the next generation.
+        if (count == 3){
+          newmatrix[i][j].alive = 1;
+        }
 
-      } else {
+        //Death by isolation: Each live cell with one or fewer live
+        //neighbors will die in the next generation.
+        if (count < 2){
+          newmatrix[i][j].alive = 0;
+        }
 
-            count = 0;
-            count += initial_state[i][j+1];
-            count += initial_state[i][j-1];
-            count += initial_state[i+1][j];
-            count += initial_state[i+1][j+1];
-            count += initial_state[i+1][j-1];
-            count += initial_state[i-1][j];
-            count += initial_state[i-1][j+1];
-            count += initial_state[i-1][j-1];
+        //Death by overcrowding: Each live cell with four or more live
+        //neighbors will die in the next generation.
+        if (count > 3){
+          newmatrix[i][j].alive = 0;
+        }
 
-            //Births: Each dead cell adjacent to exactly three live neighbors
-            //will become live in the next generation.
-            if (count == 3){
-              newmatrix[i][j] = 1;
-            }
-
-            //Death by isolation: Each live cell with one or fewer live
-            //neighbors will die in the next generation.
-            if (count < 2){
-              newmatrix[i][j] = 0;
-            }
-
-            //Death by overcrowding: Each live cell with four or more live
-            //neighbors will die in the next generation.
-            if (count > 3){
-              newmatrix[i][j] = 0;
-            }
-
-            //Survival: Each live cell with either two or three live neighbors
-            // will remain alive for the next generation.
-            if (count > 1 && count < 4){
-              newmatrix[i][j] = 1;
-            }
+        //Survival: Each live cell with either two or three live neighbors
+        // will remain alive for the next generation.
+        if (count > 1 && count < 4){
+          newmatrix[i][j].alive = 1;
+        }
       }
     }
   }
@@ -104,12 +115,12 @@ int main() {
   int i, j;
 
   // Setting up the first generation//
-  int **oldmatrix = NULL;
+  cell **oldmatrix = NULL;
   oldmatrix = setup(length);
 
   for (i = 1; i < length - 1; i++){
     for (j = 1; j < length - 1; j++){
-      oldmatrix[i][j] = (rand() % 2);
+      oldmatrix[i][j].alive = (rand() % 2);
     }
   }
 
@@ -121,7 +132,7 @@ int main() {
   char userinput[100];
 
   while((fgets(userinput, sizeof userinput, stdin) != NULL) && (userinput[0] != '\n') && userinput[0] == 'c'){
-    int** newmatrix = new_state(oldmatrix, length);
+    cell** newmatrix = new_state(oldmatrix, length);
     display(newmatrix, length);
     oldmatrix = newmatrix;
   }
