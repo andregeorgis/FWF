c3777af669aee04ccfc35564c5320fcaa3ec9e05
darius-c <darius.carlin@mcbizman.com.au>
2020-12-18 22:39:43 +1100

    Added unique cell ID and event listener when selecting a cell

--- cgolad/src/basicgame.js
+++ cgolad/src/basicgame.js
@@ -2,10 +2,22 @@ const gridContainer = document.getElementById("grid-container");
 
 function createGrid(rows, cols) {
 
-    for (i = 0; i < rows*cols; i++) {
-        let cell = document.createElement("div");
-        gridContainer.appendChild(cell).className = "cell";
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            let cell = document.createElement("div");
+            let appendedCell = gridContainer.appendChild(cell);
+            appendedCell.className = "cell";
+            appendedCell.id = `${i},${j}`;
+            appendedCell.addEventListener("click", selectCell);
+        }
     }
+}
+
+function selectCell(event) {
+    var clickedCell = event.target;
+    console.log(clickedCell);
+    clickedCell.style.backgroundColor = "red";
+
 }
 
 createGrid(10, 10);
