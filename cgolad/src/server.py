ea4732965f663ab4f0d498bbc363615e3f5c435f
darius-c <darius.carlin@mcbizman.com.au>
2021-01-04 12:05:19 +1100

    putting model grid into class

--- cgolad/src/server.py
+++ cgolad/src/server.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def main():
     return render_template('gameview.html')
 
---
+++ cgolad/src/static/grid.js
@@ -0,0 +1,18 @@
+export class Grid {
+
+    constructor() {
+        /* Implement the constructor to create a 2d square grid.
+        *  Ensure the length on this grid matches,
+        *  e.g. if the grid on the html is 10x10
+        *       the grid here should be 10x10 too.
+        */
+
+        this.grid = null;
+    }
+
+    /* Create a function to access an element of the grid, given a string.
+    *  e.g. if the supplied string is "1,3", you should access the element
+    *       this.grid[1][3].
+    *  You do not need to do any error checking on the string.
+    */
+}
