f2922512bf4f186ab19015b2790ff16fe19b7090
jtran03 <60172315+jtran03@users.noreply.github.com>
2021-01-06 10:18:36 +1100

    i have no idea what im doing

--- cgolad/src/static/grid.js
+++ cgolad/src/static/grid.js
@@ -1,12 +1,16 @@
 export class Grid {
 
-    constructor() {
+    constructor(width, height, x, y, inside) {
         /* Implement the constructor to create a 2d square grid.
         *  Ensure the length on this grid matches,
         *  e.g. if the grid on the html is 10x10
         *       the grid here should be 10x10 too.
         */
-
+        this.width = width;
+        this.height = height;
+        this.x = x;
+        this.y = y;
+        this.inside = inside;
         this.grid = null;
     }
 
